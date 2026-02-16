1eab38e0dd6c968159db4f53389a46b2d6c234b5
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-15 19:38:39 -0500

    Initial Commit

---
+++ Dungeon Dice!/Dungeon_Dice_App.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_Dice_App.swift
+//  Dungeon Dice!
+//
+//  Created by Nick Nedjat on 15.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_Dice_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

660d7dca7bec043b074d335e0e7e1b5a0ff2f1a6
Nick Nedjat <nick.nedjat@icloud.com>
2026-02-15 20:57:39 -0500

    4.4

--- Dungeon Dice!/ContentView.swift
+++ Dungeon Dice!/ContentView.swift
@@ -22,15 +22,19 @@ struct ContentView: View {
             
             Spacer()
             
-                ForEach(diceTypes, id: \.self) { diceType in
-                    Button("\(diceType)-sided") {
-                        rollDie(sides: diceType)
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 110))]){
+                    ForEach(diceTypes, id: \.self) { diceType in
+                        Button("\(diceType)-sided") {
+                            rollDie(sides: diceType)
+                        }
+                        .buttonStyle(.glassProminent)
+                        .tint(.red) // WUSSTE NICHT WIE ICH DEN HINTERGRUND VOM BUTTON EINSTELLE
+                        .lineLimit(1)
+                        .fixedSize(horizontal: true, vertical: false)
+                        .font(.title2)
                     }
-                    .buttonStyle(.glassProminent)
-                    .tint(.red) // WUSSTE NICHT WIE ICH DEN HINTERGRUND VOM BUTTON EINSTELLE
-                    .font(.title2)
+                .font(.largeTitle)
                 }
-            .font(.largeTitle)
         }
         .padding()
     }
